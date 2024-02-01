ea541a556eb061c68648145352a909a3232d1633
felipe almeida batista <felipeab10@hotmail.com>
2024-02-01 14:10:57 -0400

    adicionado docker compose

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -9,9 +9,9 @@ export default {
   out: './drizzle',
   driver: 'mysql2',
   dbCredentials: {
-    host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_HOST ?? '',
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    database: process.env.DATABASE_NAME ?? '',
   },
 } satisfies Config
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
-import { cn } from '@/lib/utils'
 import './globals.css'
 
-export const fontSans = Inter({
-  subsets: ['latin'],
-  variable: '--font-sans',
-})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -21,14 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable,
-        )}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
